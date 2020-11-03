bfbfaf6f1230e445d830bc09e9be457b1442a5dd
mariamtvalchrelidze <48021883+mariamtvalchrelidze@users.noreply.github.com>
2020-11-03 14:45:01 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
+//Globals
 var level = 0;
 var chosenPattern = [];
 var sequence = [];
 var colors = ["green", "red", "yellow", "blue"];
 var count = 0;
+var success = true;
+var randomNumber =0;
+var length = 0;
 
+//Next sequence
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
+  randomNumber = Math.floor(Math.random() * 4);
   level++;
   $("h1").text("level " + level);
   sequence.push(randomNumber);
-  var length = sequence.length;
-  sound = new Audio("sounds/" + colors[sequence[length - 1]] + ".mp3");
-  sound.play();
-  $("." + colors[sequence[length - 1]]).addClass("pressed");
-  setTimeout(function() {
-    $("." + colors[sequence[length - 1]]).removeClass("pressed");
-  }, 100);
+  length = sequence.length;
+  press(colors[sequence[length - 1]]);
   chosenPattern = [];
   count = 0;
 }
+
+//answer checker
 function checkAnswer(count) {
-  var success = true;
   if (chosenPattern[count] !== colors[sequence[count]]) {
     success = false;
   }
   return success;
 }
 
-$("body").one("keypress", function() {
+//color press
+function press(name) {
+  playSound(name);
+  $("." + name).addClass("pressed");
+  setTimeout(function() {
+    $("." + name).removeClass("pressed");
+  }, 100);
+}
+//play sound
+function playSound(name) {
+  sound = new Audio("sounds/" + name + ".mp3");
+  sound.play();
+}
+//game over
+function gameOver(){
+  playSound("wrong");
+  var currentBackground = $("body").css("background");
+  $("body").css("background-color", "red");
+  setTimeout(function() {
+    $("body").css("background", currentBackground);
+  }, 100);
+  $("h1").text("you lost");
+}
+//main
+$("body").one("keypress", function() { //if any button is clicked, game will start
   nextSequence();
-  $(".btn").click(function(event) {
-    count++;
+  $(".btn").click(function(event) {//checks if any box was clicked
+    count++;  //counts button clicks
     check = true;
-    clickedColor = $(event.target).attr("class").split(" ")[1];
-    sound = new Audio("sounds/" + clickedColor + ".mp3");
-    sound.play();
-    $("." + clickedColor).addClass("pressed");
-    setTimeout(function() {
-      $("." + clickedColor).removeClass("pressed");
-    }, 100);
-    chosenPattern.push(clickedColor);
+    chosenColor = $(event.target).attr("class").split(" ")[1];
+    press(chosenColor);
+    chosenPattern.push(chosenColor);
     for (var i = 0; i < chosenPattern.length; i++) {
-      if (checkAnswer(i)) {
+      if (checkAnswer(i)) { //checks each chosen color and compares to the actual pattern
         continue;
       } else {
         check = false;
-        sound = new Audio("sounds/wrong.mp3");
-        sound.play();
-        var currentBackground = $("body").css("background");
-        $("body").css("background-color", "red");
-        setTimeout(function() {
-          $("body").css("background", currentBackground);
-        }, 100);
-        $("h1").text("you lost");
+        gameOver();
         break;
       }
     }
     if (check) {
-      if (level === count) {
+      if (level === count) {   //if all the chosen colors match sequence and the number of colors chosen equals to level, the new box will appear
         setTimeout(function() {
           nextSequence();
         }, 1000);
